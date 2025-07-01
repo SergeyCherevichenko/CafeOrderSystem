85ee1dbd89afe837d9324ae76ae558bacc3db7d7
They Reginia <theyregi@student.21-school.ru>
2025-07-01 15:28:01 +0000

    add junit console

--- src/test/uz/cherevichenko_sergey/cafe_order_system/presenter/PresenterTest.java
+++ src/test/uz/cherevichenko_sergey/cafe_order_system/presenter/PresenterTest.java
@@ -1,13 +1,8 @@
 package uz.cherevichenko_sergey.cafe_order_system.presenter;
 
 import org.junit.jupiter.api.Test;
-import uz.cherevichenko_sergey.cafe_order_system.model.client.Client;
-import uz.cherevichenko_sergey.cafe_order_system.model.service.MenuService;
 import uz.cherevichenko_sergey.cafe_order_system.view.ConsoleUI;
 import uz.cherevichenko_sergey.cafe_order_system.view.View;
-import static org.junit.jupiter.api.Assertions.*;
-
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PresenterTest {
